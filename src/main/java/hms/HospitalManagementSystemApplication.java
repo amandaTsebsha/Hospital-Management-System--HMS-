4839b462470f0ad95cca96f47c83dd0716ca90bc
amynollz153@gmail.com <amynollz153@gmail.com>
2024-11-01 04:30:12 +0200

    HMS

---
+++ src/main/java/hms/HospitalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+// src/main/java/hms/HospitalManagementSystemApplication.java
+
+package hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementSystemApplication.class, args);
+    }
+}
